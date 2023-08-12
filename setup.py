02498da1974d1842c8d2cb3e6dcb547a219ca6a9
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2023-08-12 19:40:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='nifti2dicom',
-    version='1.0.3',
+    version='1.0.4',
     packages=find_packages(),
     install_requires=[
         'pydicom',
