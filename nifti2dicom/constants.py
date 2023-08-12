0e47db49579a83c03f65eed1b4d16c415ad3203c
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2023-08-12 23:28:15 +0200

    Update constants.py

--- nifti2dicom/constants.py
+++ nifti2dicom/constants.py
@@ -18,3 +18,43 @@ ANSI_ORANGE = '\033[38;5;208m'
 ANSI_GREEN = '\033[38;5;40m'
 ANSI_VIOLET = '\033[38;5;141m'
 ANSI_RESET = '\033[0m'
+
+
+ORGAN_INDEX = {
+    1: 'Adrenal-glands',
+    2: 'Aorta',
+    3: 'Bladder',
+    4: 'Brain',
+    5: 'Heart',
+    6: 'Kidneys',
+    7: 'Liver',
+    8: 'Pancreas',
+    9: 'Spleen',
+    10: 'Thyroid',
+    11: 'Inferior-vena-cava',
+    12: 'Lung',
+    13: 'Carpal',
+    14: 'Clavicle',
+    15: 'Femur',
+    16: 'Fibula',
+    17: 'Humerus',
+    18: 'Metacarpal',
+    19: 'Metatarsal',
+    20: 'Patella',
+    21: 'Pelvis',
+    22: 'Phalanges-of-the-hand',
+    23: 'Radius',
+    24: 'Ribcage',
+    25: 'Scapula',
+    26: 'Skull',
+    27: 'Spine',
+    28: 'Sternum',
+    29: 'Tarsal',
+    30: 'Tibia',
+    31: 'Phalanges-of-the-feet',
+    32: 'Ulna',
+    33: 'Skeletal-muscle',
+    34: 'Subcutaneous-fat',
+    35: 'Torso-fat',
+    36: 'Psoas'
+}
