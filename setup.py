3030eb5a6fd375356ad8720aa219169426eff0b1
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2023-08-12 19:25:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nifti2dicom',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     install_requires=[
         'pydicom',
