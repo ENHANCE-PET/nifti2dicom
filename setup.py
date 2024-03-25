d0bc358a2d5f636d774a0b8c0b5bf0646a2a6ab2
LalithShiyam <lalith.shiyamsundar@meduniwien.ac.at>
2024-03-25 11:29:54 +0100

    Fix version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='nifti2dicom',
-    version='1.1.9',
+    version='1.1.10',
     packages=find_packages(),
     install_requires=[
         'pydicom',
